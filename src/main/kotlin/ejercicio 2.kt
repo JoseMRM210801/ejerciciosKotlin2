6fb936f06f72d87fdf08dca2a251d0f0d57ed1e7
sharpslop <s19120164@alumnos.itsur.edu.mx>
2022-10-09 19:57:06 -0500

    mo

---
+++ src/main/kotlin/ejercicio 2.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val valores= listOf(3,5,32,6,8,28,53,10,4,100,56,87,43)
+    println(si(valores) { a -> par(a) })
+}
+
+
+fun par(valor:Int): Boolean {
+    return valor>=50
+}
+
+fun si(valores: List<Int>,par:(Int)->Boolean):List<Pair<Int,Boolean>> {
+    val vals: MutableList<Pair<Int, Boolean>> = mutableListOf()
+    for (i in valores.indices) {
+        if (par(valores[i])){
+            vals.add(Pair(valores[i], par(valores[i])))
+        }
+    }
+    return vals
+}
